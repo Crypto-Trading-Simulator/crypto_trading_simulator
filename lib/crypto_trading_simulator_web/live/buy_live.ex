22959721f0f8638c6ca898f83b2478c9cd1d334e
MustafaAldelaimi <mustafa1007@yahoo.com>
2023-03-28 13:06:33 +0100

    Buy and sell functional

--- lib/crypto_trading_simulator_web/live/buy_live.ex
+++ lib/crypto_trading_simulator_web/live/buy_live.ex
@@ -2,7 +2,6 @@ defmodule CryptoTradingSimulatorWeb.BuyLive do
   use CryptoTradingSimulatorWeb, :live_view
   alias CryptoTradingSimulator.{Repo, User, Crypto}
   require Logger
-  use Phoenix.LiveView
 
   def mount(%{"id" => id, "symbol" => symbol}, _session, socket) do
     {:ok, assign(socket, id: id, symbol: symbol, error_message: "", form: to_form(%{}))}
@@ -16,31 +15,56 @@ defmodule CryptoTradingSimulatorWeb.BuyLive do
         <.input type="number" field={@form[:amount]} />
         <.button type="submit">Buy</.button>
         <.button phx-click="sell_page">Sell Page</.button>
+        <%= @error_message %>
     </.form>
 
     """
   end
 
+  def string_to_number(string) do
+    case Integer.parse(string) do
+      {int, ""} -> int/1.0
+      _ ->
+        case Float.parse(string) do
+          {float, ""} -> float
+          _ -> 0.0
+        end
+    end
+  end
 
   def handle_event("buy", %{"amount" => amount}, socket) do
+    number_amount = string_to_number(amount)
     crypto_symbol = socket.assigns.symbol
     user_id = socket.assigns.id
-    Logger.info(socket.assigns)
+    number_user_id = trunc(string_to_number(user_id))
+
     response = HTTPoison.get!("https://min-api.cryptocompare.com/data/price?fsym=#{crypto_symbol}&tsyms=GBP")
-    price = Poison.decode!(response.body)
+    crypto_price = elem(Enum.at(Poison.decode!(response.body), 0), 1)
     user = Repo.get(User, user_id) |> Repo.preload(:cryptos)
     user_coins = user.cryptos |> Enum.map(&(&1.symbol))
     exists = Enum.any?(user_coins, fn coin_symbol -> coin_symbol == crypto_symbol end)
+
     if exists do
       temp = user.cryptos |> Enum.filter(&(&1.symbol == crypto_symbol))
       temp_2 = temp |> Enum.at(0)
-      IO.inspect(temp_2.amount)
-      # changeset = change(%Crypto{amount: temp.amount + amount})
-      # Repo.update(changeset)
-      # IO.puts(changeset, user.cryptos |> Enum.filter(&(&1.symbol == crypto_symbol)))
+      changeset = Crypto.changeset(temp_2, %{invested: temp_2.invested + number_amount ,amount: temp_2.amount + (number_amount / crypto_price)})
+      Repo.update(changeset)
+      {:noreply, assign(socket, error_message: "Successfully Bought")}
     else
+      coin_info = HTTPoison.get!("https://min-api.cryptocompare.com/data/coin/generalinfo?fsyms=#{crypto_symbol}&tsym=GBP")
+      coin_info_decoded = Poison.decode!(coin_info.body)
+      coin_fullname = Enum.at(coin_info_decoded["Data"], 0)["CoinInfo"]["FullName"]
+      Repo.insert!(
+        %Crypto{
+          coin: coin_fullname,
+          amount: (number_amount / crypto_price),
+          invested: number_amount,
+          symbol: crypto_symbol,
+          user_id: number_user_id
+        }
+      )
+      {:noreply, assign(socket, error_message: "Successfully Bought")}
     end
-    {:noreply, socket}
   end
 
   def handle_event("sell_page", _params, socket) do
--- lib/crypto_trading_simulator_web/live/sell_live.ex
+++ lib/crypto_trading_simulator_web/live/sell_live.ex
@@ -1,6 +1,6 @@
 defmodule CryptoTradingSimulatorWeb.SellLive do
   use CryptoTradingSimulatorWeb, :live_view
-  alias CryptoTradingSimulator.{Repo, User}
+  alias CryptoTradingSimulator.{Repo, User, Crypto}
   require Logger
 
   def mount(%{"id" => id, "symbol" => symbol}, _session, socket) do
@@ -10,26 +10,56 @@ defmodule CryptoTradingSimulatorWeb.SellLive do
   def render(assigns) do
     ~H"""
     <%= @symbol %>
-    <.form for={@form} phx-submit="sign_up" >
-        <h3>Name</h3>
-        <.input type="text" field={@form[:name]} />
-        <h3>Email</h3>
-        <.input type="email" field={@form[:email]} />
+    <.form for={@form} phx-submit="sell" >
+        <h3>Amount in GBP</h3>
+        <.input type="text" field={@form[:amount]} />
         <.button type="submit">Sell</.button>
         <.button phx-click="buy_page">Buy Page</.button>
+        <%= @error_message %>
     </.form>
 
     """
   end
 
+  def string_to_number(string) do
+    case Integer.parse(string) do
+      {int, ""} -> int/1.0
+      _ ->
+        case Float.parse(string) do
+          {float, ""} -> float
+          _ -> 0.0
+        end
+    end
+  end
 
-  def handle_event("sell", %{"id" => id, "symbol" => symbol, "amount" => _amount}, socket) do
-    response = HTTPoison.get!("https://min-api.cryptocompare.com/data/price?fsym=#{symbol}&tsyms=GBP")
-    data = Poison.decode!(response.body)
-    user = Repo.get(User, id) |> Repo.preload(:cryptos)
-    user_coins = user.cryptos |> Enum.map(&(&1.symbol))
-    exists = Enum.any?(user_coins, fn coin_symbol -> coin_symbol == symbol end)
-    IO.puts(exists)
+  def handle_event("sell", %{"amount" => amount}, socket) do
+    number_amount = string_to_number(amount)
+    crypto_symbol = socket.assigns.symbol
+    user_id = socket.assigns.id
+    number_user_id = trunc(string_to_number(user_id))
+
+    response = HTTPoison.get!("https://min-api.cryptocompare.com/data/price?fsym=#{crypto_symbol}&tsyms=GBP")
+    crypto_price = elem(Enum.at(Poison.decode!(response.body), 0), 1)
+    user = Repo.get(User, user_id) |> Repo.preload(:cryptos)
+    current_user_crypto = user.cryptos |> Enum.filter(&(&1.symbol == crypto_symbol)) |> Enum.at(0)
+
+    suffient? = current_user_crypto.amount - (number_amount / crypto_price)
+    cond do
+      suffient? > 0.0001 ->
+
+        changeset = Crypto.changeset(current_user_crypto, %{invested: current_user_crypto.invested - number_amount ,amount: suffient?})
+        Repo.update(changeset)
+
+        {:noreply, assign(socket, error_message: "Successfully Sold")}
+
+      suffient? <= 0.0001 ->
+        crypto = Repo.get_by(Crypto, [symbol: crypto_symbol, user_id: user_id])
+        Repo.delete!(crypto)
+        {:noreply, assign(socket, error_message: "Successfully Sold")}
+
+      true ->
+        {:noreply, assign(socket, error_message: "Insufficient Funds")}
+    end
   end
 
   def handle_event("buy_page", _params, socket) do
